f3d24eafc0d178f0f0ec0d8f5d38ee566024af2d
Frannyontech <fran.galazojeda@gmail.com>
2021-09-21 10:26:59 -0300

    new fix in filters

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :state, presence: true
+    validates :borrowed_on, presence: true
+    validates :returned_on, presence: true
+
     enum state: [:estante, :prestado]
     paginates_per 10
 end
